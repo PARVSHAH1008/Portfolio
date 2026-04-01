1bca7b50067f25c8ee7af657bbf00670b9cbe5b8
PARV SHAH <126377800+PARVSHAH1008@users.noreply.github.com>
2026-04-01 15:07:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio/", // <--- THIS IS THE KEY! It must match your repo name exactly.
   plugins: [react()],
 })
